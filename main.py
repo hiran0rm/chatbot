7e235ec8b64c07a9fc335ff0943fe41a78b688a3
nahamot <50772934+nahamot@users.noreply.github.com>
2020-06-19 19:29:51 +0900

    make it better

--- main.py
+++ main.py
@@ -34,10 +34,34 @@ def callback():
 #テキストメッセージが送信されたときの処理
 @handler.add(MessageEvent, message=TextMessage) #引数に処理したいイベントを指定してください
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='メッセージを受信しました')) #送りたいメッセージを入れてください
+    text = event.message.text
+    if text == 'おはよう':
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='おはよう'))
+    else if text == 'こんにちは':
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='こんにちは'))
+    else:
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text='メッセージを受信しました'))
 
+@handler.add(MessageEvent, message=ImageMessage)#引数に処理したいイベントを指定してください
+    def handle_message(event):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='画像を受信しました'))
+
+
+# フォローイベント時の処理
+@handler.add()  # 引数に処理したいイベントを指定してください
+def handle_follow(event):
+    # 誰が追加したかわかるように機能追加
+    profile = line_bot_api.get_profile(event.source.user_id)  # 取得したプロフィールをprofileに格納しています
+    line_bot_api.push_message(Uad6b2718be2fa3e98ecdd9e783aa83c8,
+                              TextSendMessage(text="表示名:{}\ユーザID:{}\n画像のURL:{}\nステータスメッセージ:{}" \
+                                              .format(profile.display_name, profile.user_id, profile.picture_url,
+                                                      profile.status_message)))
+
+    # 友達追加したユーザにメッセージを送信
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text='友達追加ありがとうございます'))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
