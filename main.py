540359a4fe0d35a6268a4d2765cd958a66c163cf
nahamot <50772934+nahamot@users.noreply.github.com>
2020-06-23 17:35:29 +0900

    make it better

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ from linebot.models import (
     MessageEvent, TextMessage,ImageMessage, TextSendMessage, FollowEvent
 )
 
+# 画像認識のためのインポート
+from keras.models import Sequential, load_model
+from keras.preprocessing import image
+import tensorflow as tf
+import numpy as np
+
+# 画像認識のためのコード
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+try:
+    os.makedirs(static_tmp_path)
+except OSError as exc:
+    if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
+        pass
+    else:
+        raise
+
+
+# 普通の機能用
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('85GuDL0mm/fktE/4QksofPV5VrmZQVbS2/VhT+F1wiu356SJk/lLyXUN+F4vqEr8Dd60RnjR32/7sQ0vcPvOrXUMUpoegYOXR6S04McNNJ1cn1CwwZyc7aHkC396KD+iszTsSPN5yKUXu752goSzGwdB04t89/1O/w1cDnyilFU=') #アクセストークンを入れてください
@@ -31,7 +49,7 @@ def callback():
 
     return 'OK'
 
-#テキストメッセージが送信されたときの処理
+# テキストメッセージが送信されたときの処理
 @handler.add(MessageEvent, message=TextMessage) #引数に処理したいイベントを指定してください
 def handle_message(event):
     text = event.message.text
@@ -39,14 +57,41 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='おはようございます!'))
     elif text == 'こんにちは':
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='こんにちは！'))
+    elif text == '?':
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='だが断る！'))
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
-@handler.add(MessageEvent, message=ImageMessage)#引数に処理したいイベントを指定してください
-def handle_message(event):
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='画像を受信しました'))
+# 以下、送信された画像を保存するためのコード
+class_label = ["飛行機","自動車","鳥","猫","鹿","犬","蛙","馬","船","トラック"]
+graph = tf.get_default_graph()# kerasのバグでこのコードが必要.
+model = load_model('my_model.h5')# 学習済みモデルをロードする
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_content_message(event):
+    global graph
+    with graph.as_default():
+        message_content = line_bot_api.get_message_content(event.message.id)
+        with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix="jpg" + '-', delete=False) as tf:
+            for chunk in message_content.iter_content():
+                tf.write(chunk)
+                tempfile_path = tf.name
+
+        dist_path = tempfile_path + '.' + "jpg"
+        dist_name = os.path.basename(dist_path)
+        os.rename(tempfile_path, dist_path)
+
+        filepath = os.path.join('static', 'tmp', dist_name) # 送信された画像のパスが格納されている
+
+# 以下、送信された画像をモデルに入れる
+        img = image.load_img(filepath, target_size=(32, 32)) # 送信された画像を読み込み、リサイズする
+        img = image.img_to_array(img) # 画像データをndarrayに変換する
+        data = np.array([img]) # model.predict()で扱えるデータの次元にそろえる
+
+        result = model.predict(data)
+        predicted = result.argmax() # 予測結果が格納されている
+        pred_answer = "これは" + class_label[predicted] + "です。"
+        line_bot_api.reply_message(event.reply_token, [TextSendMessage(text=pred_answer)])
 
 
 # フォローイベント時の処理
